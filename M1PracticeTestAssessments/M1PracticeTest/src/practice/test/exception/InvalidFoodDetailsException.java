c34f883edacc9060f4c584476078e2f010f49687
Pavansaiganta <144276708+Pavansaiganta@users.noreply.github.com>
2023-10-13 12:37:41 +0530

    Add files via upload

---
+++ M1PracticeTestAssessments/M1PracticeTest/src/practice/test/exception/InvalidFoodDetailsException.java
@@ -0,0 +1,8 @@
+package practice.test.exception;
+
+public class InvalidFoodDetailsException extends Exception {
+
+ public InvalidFoodDetailsException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ M1PracticeTestAssessments/M1PracticeTest/src/practice/test/exception/InvalidFoodRegionException.java
@@ -0,0 +1,11 @@
+package practice.test.exception;
+
+public class InvalidFoodRegionException  extends InvalidFoodDetailsException{
+
+	public InvalidFoodRegionException(String s) {
+		super(s);
+	}
+	
+}
+
+
